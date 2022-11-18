2b6fef7d65a584bc0031a82de0e7b68c81ed92f9
Robin <robin.escaffre@edu.esiee.fr>
2022-11-18 21:22:49 +0100

    Base du jeu (VR+Normal)

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -7,13 +7,32 @@ public class move : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 12f;
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public bool isGrounded;
+
+    Vector3 velocity;
     private void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
