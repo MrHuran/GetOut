1c3e79ac41f7be912cad3e24a5a0f7559d4f1a89
MrHuran <poulain4422@gmail.com>
2023-05-27 11:17:57 +0200

    MonstreDeplacement FINI + SceneNonVR MIS A JOUR

---
+++ Assets/Scripts/MonsterAI.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterAI : MonoBehaviour
+{
+    public NavMeshAgent monster;
+
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    Animator animator;
+
+    //Variables pour la patrouille
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Variables pour l'état du monstre
+    public float sightRange;
+    public bool playerInSightRange;
+
+    //Variable pour checker si le monstre se déplace ou non
+    Vector3 prevPos;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        player = GameObject.Find("Player").transform;
+        monster = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        //Check si le joueur est dans la vision du monstre
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+
+        if (!playerInSightRange) Patroling();
+        else Chasing();
+    }
+
+    private void Patroling()
+    {
+        // Si le monstre ne sait pas où aller, alors il cherche un endroit où aller
+        if (!walkPointSet)
+        {
+            animator.SetBool("isMoving", false);
+            animator.SetBool("isChasing", false);
+            SearchWalkPoint();
+        }
+        else
+        {
+            animator.SetBool("isMoving", true);
+            animator.SetBool("isChasing", false);
+            monster.SetDestination(walkPoint);
+            //Si le monstre est bloqué (ne bouge pas), alors il cherche un autre endroit où aller
+            if (transform.position == prevPos)
+            {
+                Debug.Log("Le monstre est bloqué");
+                animator.SetBool("isMoving", false);
+                animator.SetBool("isChasing", false);
+                SearchWalkPoint();
+            }
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //WalkPoint atteint
+        if (distanceToWalkPoint.magnitude < 1f) walkPointSet = false;
+
+        prevPos = transform.position;
+    }
+
+    private void SearchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.y + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround)) walkPointSet = true;
+    }
+
+    private void Chasing()
+    {
+        animator.SetBool("isChasing", true);
+        animator.SetBool("isMoving", true);
+        monster.SetDestination(player.position);
+        walkPointSet = false;
+    }
+}
